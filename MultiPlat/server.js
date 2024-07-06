4929ecc6e26ba27b83e5cd6c51558ce1a8068105
Louis-Charles Biron <louilresbiron6@gmail.com>
2024-07-06 11:23:48 -0400

    Init project

--- MultiPlat/server.js
+++ MultiPlat/server.js
@@ -1,8 +1,53 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+"use strict";
+const express = require("express"),
+    fs = require("fs"),
+    bodyParser = require("body-parser"),
+    webSocket = require("ws")
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const app = express()
+app.use(bodyParser.json())
+
+// REQUESTS
+app.get("/", (req, res) => {
+    fs.readFile(__dirname+"/public/index.html", "utf8", (err, data) => 
+        res.send(
+	        data.replaceAll("{{IP}}", req.ip)
+        )
+    )
+})
+
+app.post("/template", (req, res) => {
+    console.log(req.body.value)
+})
+
+// SERVER
+const server = app.listen(3000, "0.0.0.0", () => console.log("server up")),
+    wss = new webSocket.Server({ server })
+    app.use(express.static(__dirname+"/public"))
+
+// WEB SOCKET
+let idGiver = 0
+wss.on("connection", (ws, req) => {
+    let ip = req.socket.remoteAddress
+    ws.info = { id: idGiver++, ip: ip }
+
+    console.log("CONNECTION: " + ip)
+
+    ws.on("close", () => {
+        console.log("DISCONNECT: " + ip)
+    })
+
+    ws.on("message", m => {
+        console.log(m.toString())
+    })
+})
+
+
+function wsSendAll(obj) {
+    wss.clients.forEach((c) => {
+        c.send(JSON.stringify(obj))
+    })
+}
+function wsSend(ws, obj) {
+    ws.send(JSON.stringify(obj))
+}
