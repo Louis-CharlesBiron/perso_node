6c6a308c021cc8b8188b2e6505cc67f3fd24225f
Louis-Charles Biron <louilresbiron6@gmail.com>
2024-11-24 09:32:09 -0500

    Continue tcgaille project

--- Gaille/src/index.js
+++ Gaille/src/index.js
@@ -1,6 +1,36 @@
+const canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d")
+ctx.strokeWidth = 5
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
 
+window.addEventListener("resize", resizeCanvas)
+resizeCanvas()
 
+let isdrawing=false, lastX=null, lastY=null
 
-console.log("Hi")
-send({type:"hi to backend"})
+canvas.onmousemove=e=>{
+    if (isdrawing) {
+        let x = e.clientX, y = e.clientY
+
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+        ctx.lineTo(lastX||x, lastY||y)
+        ctx.strokeStyle = "red"
+        ctx.stroke()
+    
+        lastX=x
+        lastY=y
+    }
+}
+
+canvas.onmousedown=()=>{
+  isdrawing = true
+}
+
+canvas.onmouseup=()=>{
+  isdrawing = false
+  lastX = lastY = null
+}
